b50c935760511d1bc258fa997da883ec48b37cdf
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-06-22 06:17:36 +0700

    added block colour

---
+++ include/Block/BlockColour.hpp
@@ -0,0 +1,14 @@
+#ifndef NANARO_BLOCK_BLOCKCOLOUR_HEAD_HPP
+#define NANARO_BLOCK_BLOCKCOLOUR_HEAD_HPP
+#include <cstdint>
+
+namespace Block {
+  enum class Color {
+                    NO,
+                    YES,
+                    RED,
+                    GREEN
+  }
+}
+
+#endif //NANARO_BLOCK_BLOCKCOLOUR_HEAD_HPP
