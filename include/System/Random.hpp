e79f821f490a2244daf51166bf72773f36f64024
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-06-25 01:42:46 +0700

    added the random number generator for wall

--- include/System/Random.hpp
+++ include/System/Random.hpp
@@ -1,9 +1,12 @@
 #ifndef NANARO_SYSTEM_RANDOM_HEAD_HPP
 #define NANARO_SYSTEM_RANDOM_HEAD_HPP
+#include <cstddef>
 
 namespace sys {
   namespace rng {
     // TODO : Set the get and maybe initializer for random. Could also be a class with static function
+    void init(void);
+    std::size_t wall(void); // 0 <= x < 4
   }
 }
 
