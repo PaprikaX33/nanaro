936a1021659bf5bb75d1d246b5ec3a426ff56cd4
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-06-25 03:42:53 +0700

    completed a naive wall build

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 
 void write_separator(enum Block::Type * );
+void draw_border(enum Block::Type * );
 
 int main(int argc, char ** argv)
 {
@@ -19,13 +20,7 @@ int main(int argc, char ** argv)
   sf::View viewScaler(sf::FloatRect(0, 0, 360, 264)); //Not an exact square, but good enough
   enum Block::Type testingGrid[660u];
   for(std::size_t i = 0; i < 660u; i++){
-    switch(sys::rng::wall()){
-    case 0: testingGrid[i] = Block::Type::WALL_A; break;
-    case 1: testingGrid[i] = Block::Type::WALL_B; break;
-    case 2: testingGrid[i] = Block::Type::WALL_C; break;
-    default:
-    case 3: testingGrid[i] = Block::Type::WALL_D; break;
-    }
+    testingGrid[i] = Block::Type::WHITE;
   }
   testingGrid[30+1] = Block::Type::ZERO;
   testingGrid[30+2] = Block::Type::ONE;
@@ -37,6 +32,7 @@ int main(int argc, char ** argv)
   testingGrid[30+8] = Block::Type::SEVEN;
   testingGrid[30+9] = Block::Type::NINE;
   write_separator(testingGrid);
+  draw_border(testingGrid);
   window.setView(viewScaler);
   window.setFramerateLimit(60);
   while(window.isOpen()){
@@ -68,4 +64,41 @@ void write_separator(enum Block::Type * arr)
     arr[i + (30 * 17)] = Block::Type::BLACK;
     arr[i + (30 * 21)] = Block::Type::BLACK;
   }
+}
+
+void draw_border(enum Block::Type * arr)
+{
+  for(std::size_t i = 0; i < 16u; i++){
+    switch(sys::rng::wall()){
+    default:
+    case 0: arr[30+13+i] = Block::Type::WALL_A; break;
+    case 1: arr[30+13+i] = Block::Type::WALL_B; break;
+    case 2: arr[30+13+i] = Block::Type::WALL_C; break;
+    case 3: arr[30+13+i] = Block::Type::WALL_D; break;
+    }
+    switch(sys::rng::wall()){
+    default:
+    case 0: arr[480+13+i] = Block::Type::WALL_A; break;
+    case 1: arr[480+13+i] = Block::Type::WALL_B; break;
+    case 2: arr[480+13+i] = Block::Type::WALL_C; break;
+    case 3: arr[480+13+i] = Block::Type::WALL_D; break;
+    }
+  }
+  for(std::size_t i = 2; i < 16u; i++){
+    switch(sys::rng::wall()){
+    default:
+    case 0: arr[30*i+13] = Block::Type::WALL_A; break;
+    case 1: arr[30*i+13] = Block::Type::WALL_B; break;
+    case 2: arr[30*i+13] = Block::Type::WALL_C; break;
+    case 3: arr[30*i+13] = Block::Type::WALL_D; break;
+    }
+    switch(sys::rng::wall()){
+    default:
+    case 0: arr[30*i+28] = Block::Type::WALL_A; break;
+    case 1: arr[30*i+28] = Block::Type::WALL_B; break;
+    case 2: arr[30*i+28] = Block::Type::WALL_C; break;
+    case 3: arr[30*i+28] = Block::Type::WALL_D; break;
+    }
+  }
+  return;
 }
