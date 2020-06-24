af56e5e0999e4ef2e9d2eab92198db1f0aa9fa58
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-06-25 04:09:20 +0700

    completed the grid opening, but still wrong

--- src/grid/wall.cpp
+++ src/grid/wall.cpp
@@ -1,6 +1,16 @@
 #include "Grid/Wall.hpp"
 #include "Grid/Type.hpp"
 #include "System/Random.hpp"
+#include <cstdint>
+
+enum class OpPoss : std::uint_least8_t {
+                                        TOP,
+                                        RGH,
+                                        BOT,
+                                        LFT
+};
+
+static void create_openings(enum Block::Type * arr, enum OpPoss pos);
 
 void Grid::Border::draw(enum Block::Type * arr, enum Grid::Border::Type type)
 {
@@ -36,6 +46,63 @@ void Grid::Border::draw(enum Block::Type * arr, enum Grid::Border::Type type)
     case 3: arr[30*i+28] = Block::Type::WALL_D; break;
     }
   }
-  (void)type;
+  switch(type) {
+  case Grid::Border::Type::TOP_LFT:
+    create_openings(arr, OpPoss::RGH);
+    create_openings(arr, OpPoss::BOT);
+    break;
+  case Grid::Border::Type::TOP_CNT:
+    create_openings(arr, OpPoss::RGH);
+    create_openings(arr, OpPoss::BOT);
+    create_openings(arr, OpPoss::LFT);
+    break;
+  case Grid::Border::Type::TOP_RGH:
+    create_openings(arr, OpPoss::BOT);
+    create_openings(arr, OpPoss::LFT);
+    break;
+  case Grid::Border::Type::MID_LFT:
+    create_openings(arr, OpPoss::TOP);
+    create_openings(arr, OpPoss::RGH);
+    create_openings(arr, OpPoss::BOT);
+    break;
+  case Grid::Border::Type::MID_CNT:
+    create_openings(arr, OpPoss::TOP);
+    create_openings(arr, OpPoss::RGH);
+    create_openings(arr, OpPoss::BOT);
+    create_openings(arr, OpPoss::LFT);
+    break;
+  case Grid::Border::Type::MID_RGH:
+    create_openings(arr, OpPoss::TOP);
+    create_openings(arr, OpPoss::BOT);
+    create_openings(arr, OpPoss::LFT);
+    break;
+  case Grid::Border::Type::BOT_LFT:
+    create_openings(arr, OpPoss::TOP);
+    create_openings(arr, OpPoss::RGH);
+    break;
+  case Grid::Border::Type::BOT_CNT:
+    create_openings(arr, OpPoss::TOP);
+    create_openings(arr, OpPoss::RGH);
+    create_openings(arr, OpPoss::LFT);
+    break;
+  case Grid::Border::Type::BOT_RGH:
+    create_openings(arr, OpPoss::TOP);
+    create_openings(arr, OpPoss::LFT);
+    break;
+  }
   return;
+}
+
+void create_openings(enum Block::Type * arr, enum OpPoss pos)
+{
+  //20,21
+  for(std::size_t i = 0u; i < 2u; i++){
+    switch(pos){
+    default:
+    case OpPoss::TOP: arr[30+20+i] = Block::Type::BLANK; break;
+    case OpPoss::RGH: arr[30*(i+20)+28] = Block::Type::BLANK; break;
+    case OpPoss::BOT: arr[480+20+i] = Block::Type::BLANK; break;
+    case OpPoss::LFT: arr[30*(i+20)+13] = Block::Type::BLANK; break;
+    }
+  }
 }
