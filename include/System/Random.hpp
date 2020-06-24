87425c0b5dd6580c4e3fbed70bce3876e11ac34b
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-06-24 21:30:17 +0700

    added the random skeleton

---
+++ include/System/Random.hpp
@@ -0,0 +1,10 @@
+#ifndef NANARO_SYSTEM_RANDOM_HEAD_HPP
+#define NANARO_SYSTEM_RANDOM_HEAD_HPP
+
+namespace sys {
+  namespace rng {
+    // TODO : Set the get and maybe initializer for random. Could also be a class with static function
+  }
+}
+
+#endif //NANARO_SYSTEM_RANDOM_HEAD_HPP
