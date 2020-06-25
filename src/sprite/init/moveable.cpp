5526418e9d8ea27deeac90e278342e783680cb1a
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-06-25 19:35:10 +0700

    added the skeleton for the player and enemy

--- src/sprite/init/moveable.cpp
+++ src/sprite/init/moveable.cpp
@@ -6,22 +6,50 @@
 
 void Sprite::Init::enemy(Block::Colour * arr)
 {
-  (void)arr;
-  //sf::Texture texture;
+  sf::Texture texture;
   //ENEMY_UP
+  reset_col(arr);
+  texture.loadFromImage(Block::to_image(arr));
+  Sprite::textureMap[Block::Type::ENEMY_UP] = texture;
   //ENEMY_LEFT
+  reset_col(arr);
+  texture.loadFromImage(Block::to_image(arr));
+  Sprite::textureMap[Block::Type::ENEMY_LEFT] = texture;
   //ENEMY_DOWN
+  reset_col(arr);
+  texture.loadFromImage(Block::to_image(arr));
+  Sprite::textureMap[Block::Type::ENEMY_DOWN] = texture;
   //ENEMY_RIGHT
+  reset_col(arr);
+  texture.loadFromImage(Block::to_image(arr));
+  Sprite::textureMap[Block::Type::ENEMY_RIGHT] = texture;
   //ENEMY_HIT
+  reset_col(arr);
+  texture.loadFromImage(Block::to_image(arr));
+  Sprite::textureMap[Block::Type::ENEMY_HIT] = texture;
 }
 
 void Sprite::Init::player(Block::Colour * arr)
 {
-  (void)arr;
   sf::Texture texture;
   //PLAYER_UP
+  reset_col(arr);
+  texture.loadFromImage(Block::to_image(arr));
+  Sprite::textureMap[Block::Type::PLAYER_UP] = texture;
   //PLAYER_LEFT
+  reset_col(arr);
+  texture.loadFromImage(Block::to_image(arr));
+  Sprite::textureMap[Block::Type::PLAYER_LEFT] = texture;
   //PLAYER_DOWN
+  reset_col(arr);
+  texture.loadFromImage(Block::to_image(arr));
+  Sprite::textureMap[Block::Type::PLAYER_DOWN] = texture;
   //PLAYER_RIGHT
+  reset_col(arr);
+  texture.loadFromImage(Block::to_image(arr));
+  Sprite::textureMap[Block::Type::PLAYER_RIGHT] = texture;
   //PLAYER_HIT
+  reset_col(arr);
+  texture.loadFromImage(Block::to_image(arr));
+  Sprite::textureMap[Block::Type::PLAYER_HIT] = texture;
 }
