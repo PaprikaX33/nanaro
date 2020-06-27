1a202e1ecab5cf5ba5fc54bf05d216a523ddaeba
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-06-28 06:14:22 +0700

    added the main implementation file for player

--- include/Player.hpp
+++ include/Player.hpp
@@ -2,23 +2,25 @@
 #define NANARO_PLAYER_PROPERTIES_HEAD_HPP
 #include "System/Pair.hpp"
 #include "System/Direction.hpp"
+#include "Block/Type.hpp"
 
-namespace Ply {
-  struct Player{
-    enum class Action {
-                       MOVE_TOP,
-                       MOVE_RGH,
-                       MOVE_BOT,
-                       MOVE_LFT,
-                       ATTACK_RNG,
-                       ATTACK_MLE,
-                       HIT
-    };
-    sys::Pairf pos;
-    enum sys::Dir direction;
-    void state_update(Action);
-    void game_display_draw(Block::Type *) const;
+struct Player{
+  enum class Action {
+                     MOVE_TOP,
+                     MOVE_RGH,
+                     MOVE_BOT,
+                     MOVE_LFT,
+                     ATTACK_RNG,
+                     ATTACK_MLE
   };
-}
+  sys::Pairf pos;
+  enum sys::Dir direction;
+  void state_update(Action);
+  void game_display_draw(Block::Type *) const;
+  //void hit(void);
+  Player();
+  ~Player();
+};
+
 
 #endif //NANARO_PLAYER_PROPERTIES_HEAD_HPP
---
+++ src/player.cpp
@@ -0,0 +1,9 @@
+#include "Player.hpp"
+
+Player::Player():
+  pos{7,7},
+  direction{sys::Dir::RGH}
+{
+}
+
+Player::~Player(){}
