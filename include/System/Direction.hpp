c6896d48c491b77fbd535043c25c17931e20dead
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-06-28 05:35:22 +0700

    added the direction enum

---
+++ include/System/Direction.hpp
@@ -0,0 +1,13 @@
+#ifndef NANARO_SYSTEM_FACING_DIRECTION_HEAD_HPP
+#define NANARO_SYSTEM_FACING_DIRECTION_HEAD_HPP
+
+namespace sys {
+  enum class Dir {
+                  TOP,
+                  RHT,
+                  BOT,
+                  LFT
+  };
+}
+
+#endif //NANARO_SYSTEM_FACING_DIRECTION_HEAD_HPP
