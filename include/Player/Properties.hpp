cc5e089c0398b8943b4724fad5baf28f5f8c6f21
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-06-28 06:05:34 +0700

    completed the player specification

--- include/Player/Properties.hpp
+++ include/Player/Properties.hpp
@@ -5,8 +5,19 @@
 
 namespace Ply {
   struct Player{
+    enum class Action {
+                       MOVE_TOP,
+                       MOVE_RGH,
+                       MOVE_BOT,
+                       MOVE_LFT,
+                       ATTACK_RNG,
+                       ATTACK_MLE,
+                       HIT
+    };
     sys::Pairf pos;
     enum sys::Dir direction;
+    void state_update(Action);
+    void game_display_draw(Block::Type *) const;
   };
 }
 
