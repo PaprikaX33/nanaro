509877381dae99db620e3b8209e98a123731df09
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-06-29 05:57:46 +0700

    added the grid properties skeleton

---
+++ include/Grid/Properties.hpp
@@ -0,0 +1,9 @@
+#ifndef NANARO_GRID_PROPERTIES_HEAD_HPP
+#define NANARO_GRID_PROPERTIES_HEAD_HPP
+
+namespace Grid {
+  //TODO : GRID OBJECT
+  extern std::unordered_map<std::pair<int,int> GRID::OBJECT> textureMap;
+}
+
+#endif //NANARO_GRID_PROPERTIES_HEAD_HPP
