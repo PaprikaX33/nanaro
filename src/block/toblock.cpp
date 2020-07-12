a40e48928473094c51967b4a55e88a82bc5191f5
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-07-12 18:18:45 +0700

    added the char to block

--- src/block/toblock.cpp
+++ src/block/toblock.cpp
@@ -18,4 +18,19 @@ Block::Type Block::conv::num(std::size_t val)
 }
 
 
-Block::Type Block::conv::chr(char);
+Block::Type Block::conv::chr(char val)
+{
+  switch(val){
+  case '0' : return Block::Type::ZERO;
+  case '1' : return Block::Type::ONE;
+  case '2' : return Block::Type::TWO;
+  case '3' : return Block::Type::THREE;
+  case '4' : return Block::Type::FOUR;
+  case '5' : return Block::Type::FIVE;
+  case '6' : return Block::Type::SIX;
+  case '7' : return Block::Type::SEVEN;
+  case '8' : return Block::Type::EIGHT;
+  case '9' : return Block::Type::NINE;
+  default: return Block::Type::INVALID;
+  }
+}
