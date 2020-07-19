20487b7973645775cb2904d8d9bb39c3ac390516
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-07-19 07:05:43 +0700

    added the file for the grid

---
+++ include/Grid/Type.hpp
@@ -0,0 +1,9 @@
+#ifndef NANARO_GRID_TYPE_HEAD_HPP
+#define NANARO_GRID_TYPE_HEAD_HPP
+#include <array>
+
+namespace Grid {
+  using GridWallLayout = std::array<bool, 256u>; //An array of wall or not
+}
+
+#endif //NANARO_GRID_TYPE_HEAD_HPP
---
+++ src/grid/type.cpp
@@ -0,0 +1,4 @@
+#include "Grid/Type.hpp"
+#include <set>
+
+static std::set<Grid::GridWallLayout> gridLayout;
