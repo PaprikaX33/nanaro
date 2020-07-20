44adbf5630bf68950f64d175604b357f7b8836b5
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-07-21 06:42:17 +0700

    moved the naming system

---
+++ include/Grid/WallLayout.hpp
@@ -0,0 +1,14 @@
+#ifndef NANARO_GRID_TYPE_HEAD_HPP
+#define NANARO_GRID_TYPE_HEAD_HPP
+#include <array>
+
+namespace Grid {
+  namespace Wall{
+    using Layout = std::array<bool, 256u>; //An array of wall or not
+    void initialize(void); //An initialize function for the handwritten walling pattern
+    ::Grid::Wall::Layout const & get_wall_layout(void); //get random layout
+    //TODO: get a controlable layout
+    }
+}
+
+#endif //NANARO_GRID_TYPE_HEAD_HPP
--- src/grid/type.cpp
+++
@@ -1,4 +0,0 @@
-#include "Grid/Type.hpp"
-#include <set>
-
-static std::set<Grid::GridWallLayout> gridLayout;
