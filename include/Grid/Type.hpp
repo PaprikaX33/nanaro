d50a80470c3488bfbcbc8882012daf9f997806ff
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-07-21 06:38:16 +0700

    added the skeleton file

--- include/Grid/Type.hpp
+++ include/Grid/Type.hpp
@@ -5,7 +5,7 @@
 namespace Grid {
   using GridWallLayout = std::array<bool, 256u>; //An array of wall or not
   void initialize(void); //An initialize function for the handwritten walling pattern
-  GridWallLayout const & get_layout(void) //get random layout
+  GridWallLayout const & get_wall_layout(void) //get random layout
   //TODO: get a controlable layout
 }
 
