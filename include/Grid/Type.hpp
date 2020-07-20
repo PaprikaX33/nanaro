5cceeb1734ca5b0b7d11b2d0f8d70912ab3bfd94
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-07-21 06:37:24 +0700

    added the function to generate the grid layout

--- include/Grid/Type.hpp
+++ include/Grid/Type.hpp
@@ -4,6 +4,9 @@
 
 namespace Grid {
   using GridWallLayout = std::array<bool, 256u>; //An array of wall or not
+  void initialize(void); //An initialize function for the handwritten walling pattern
+  GridWallLayout const & get_layout(void) //get random layout
+  //TODO: get a controlable layout
 }
 
 #endif //NANARO_GRID_TYPE_HEAD_HPP
