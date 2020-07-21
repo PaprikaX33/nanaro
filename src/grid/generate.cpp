e391791771ceab951da4772602449b586b199a6e
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-07-21 07:11:59 +0700

    added the random wall adder in the grid

--- src/grid/generate.cpp
+++ src/grid/generate.cpp
@@ -1,6 +1,7 @@
 #include "Grid/Layout.hpp"
 #include "Grid/Properties.hpp"
 #include "Grid/Wall.hpp"
+#include "Grid/WallLayout.hpp"
 #include "Grid/Generate.hpp"
 #include "Block/Type.hpp"
 #include "Block/ToBlock.hpp"
@@ -17,18 +18,34 @@ void Grid::generate_grid(std::size_t count)
 {
   Grid::textureMap.clear();
   auto const layout = Grid::generate_layout(count + 1);
+
+  auto getRandomWallType = [](void){
+                             switch(sys::rng::wall()){
+                             default:
+                             case 0: return Block::Type::WALL_A;
+                             case 1: return Block::Type::WALL_B;
+                             case 2: return Block::Type::WALL_C;
+                             case 3: return Block::Type::WALL_D;
+                             }
+                           };
+
   for(auto const & position : layout) {
     std::array<Block::Type, Grid::gameArrSize> mainArr;
     auto const borderType = get_border_type(position, layout);
-    for(auto & u : mainArr) {
-      u = Block::Type::BLANK;
+    // for(auto & u : mainArr) {
+    //   u = Block::Type::BLANK;
+    // }
+    //mainArr[16u+13] = Block::conv::num(static_cast<std::size_t>(position.first < 0 ? position.first * (-1) : position.first));
+    //mainArr[16u+14] = Block::conv::num(static_cast<std::size_t>(position.second < 0 ? position.second * (-1) : position.second));
+    //mainArr[2u*16u+13] = position.first < 0 ? Block::Type::RED : Block::Type::GREEN;
+    //mainArr[2u*16u+14] = position.second < 0 ? Block::Type::RED : Block::Type::GREEN;
+    //mainArr[8u*16u+8] = Block::Type::WALL_A;
+    auto const wallLay = Grid::Wall::get_random_layout();
+    for(std::size_t x = 0; x < 14u; x++){
+      for(std::size_t y = 0; y < 14u; y++){
+        mainArr[x+1+((y+1)*16u)] = wallLay[x+(y*14u)] ? getRandomWallType() : Block::Type::BLANK;
+      }
     }
-    mainArr[16u+13] = Block::conv::num(static_cast<std::size_t>(position.first < 0 ? position.first * (-1) : position.first));
-    mainArr[16u+14] = Block::conv::num(static_cast<std::size_t>(position.second < 0 ? position.second * (-1) : position.second));
-    mainArr[2u*16u+13] = position.first < 0 ? Block::Type::RED : Block::Type::GREEN;
-    mainArr[2u*16u+14] = position.second < 0 ? Block::Type::RED : Block::Type::GREEN;
-    mainArr[8u*16u+8] = Block::Type::WALL_A;
-
 
     Grid::Border::draw(mainArr.data(), borderType);
     Grid::textureMap[position] = mainArr;
